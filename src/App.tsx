dda2300aa30154e7f5b884617c173973b7128ca3
taoyouqian <929913084@qq.com>
2021-05-27 17:40:20 +0800

    feat(components): 引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'antd/dist/antd.less'
+import {Button} from 'antd'
+import SiderDemo from './components/Meun'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const btnClick = ()=>{
+    setCount((v)=> 1 + v)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <SiderDemo></SiderDemo>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+        // 重写 less 变量，定制样式
+        modifyVars: {
+
+        },
+      },
+    }
+  }
 })
