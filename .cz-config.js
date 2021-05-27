3eda8e626f98730b06d3f20ab10ebff8066f30a2
taoyouqian <929913084@qq.com>
2021-05-27 17:06:36 +0800

    fix(deps): eslint

--- .cz-config.js
+++ .cz-config.js
@@ -4,16 +4,26 @@ module.exports = {
     { value: 'feat', name: 'feat:     新增功能' },
     { value: 'fix', name: 'fix:      修复 bug' },
     { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
-    { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
+    {
+      value: 'style',
+      name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）'
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
+    },
     { value: 'perf', name: 'perf:     性能优化' },
     { value: 'test', name: 'test:     添加、修改测试用例' },
     {
       value: 'build',
-      name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
+      name:
+        'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
     },
     { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
+    {
+      value: 'chore',
+      name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
+    },
     { value: 'revert', name: 'revert:   回滚 commit' }
   ],
 
@@ -75,9 +85,8 @@ module.exports = {
   // 跳过要询问的步骤
   // skipQuestions: ['body', 'footer'],
 
-  // subject 限制长度
-  subjectLimit: 100,
-  breaklineChar: '|' // 支持 body 和 footer
+  subjectLimit: 100, // subject 限制长度
+  breaklineChar: '|' // 换行符，支持 body 和 footer
   // footerPrefix : 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true,
 }
--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,19 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {}
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.tsx', '.ts']
+      }
+    ],
+    'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-unresolved': [2, { ignore: ['antd-mobile'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  }
 }
